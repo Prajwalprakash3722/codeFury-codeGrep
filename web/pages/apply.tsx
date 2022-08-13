721a761f00a42c4b0f66e99fbf57247620a4274f
sannidhi-s-shetty <sannidhishetty30@gmail.com>
2022-08-13 12:32:10 +0530

    q

--- web/pages/apply.tsx
+++ web/pages/apply.tsx
@@ -2,13 +2,20 @@ import type { NextPage } from "next";
 import Job_title from "../components/Job";
 
 const About: NextPage = () => {
-  return (
-    <>
-      <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-      <Job_title/>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+                <Job_title />
+                <Job_title />
+                <Job_title />
+                <Job_title />
+                <Job_title />
+                <Job_title />
+                <Job_title />
+
+            </div>
+        </>
+    );
 };
 
 export default About;
