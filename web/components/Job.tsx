193a5d9a18c0bef90e1df26277025e0bb07039e1
sannidhi-s-shetty <sannidhishetty30@gmail.com>
2022-08-13 12:04:43 +0530

    added component Job_title

---
+++ web/components/Job.tsx
@@ -0,0 +1,13 @@
+import { Grid } from '@mantine/core';
+
+function Job_title() {
+    return (
+        <Grid>
+            <Grid.Col md={6} lg={3}>
+                hg
+            </Grid.Col>
+        </Grid>
+    );
+}
+
+export default Job_title
