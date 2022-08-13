86fef1bbebd1bc345da3569e91f26a8399c8d3ac
sannidhi-s-shetty <sannidhishetty30@gmail.com>
2022-08-13 12:29:04 +0530

    changed jo-title

---
+++ web/components/Buttons.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@mantine/core';
+
+function Buttons() {
+  return (
+    <>
+      <Button variant="gradient" gradient={{ from: 'orange', to: 'red' }}>Apply</Button>
+    </>
+  );
+}
+export default Buttons;
